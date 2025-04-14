eaf604a8706f7d09a06b06aefb1f6be66c9415e9
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-04-14 01:33:24 -0700

    added first class

--- Found a Crystal/CrystalManager.cs
+++ Found a Crystal/CrystalManager.cs
@@ -1,6 +1,38 @@
-﻿namespace Found_a_Crystal;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Found_a_Crystal;
 
 public class CrystalManager
 {
-    
+    public List<string> CrystalTypes = new List<string>
+    {
+        "Cubic",
+        "Tetragonal",
+        "Orthorhombic",
+        "Monoclinic",
+        "Triclinic",
+        "Rhombohedral",
+        "Hexagonal",
+        "Not Sure?"
+    };
+    public void AddCrystals(Dictionary<string, List<string>> collection)
+    {
+        Console.Write("Enter location: ");
+        string location = Console.ReadLine();
+
+        Console.Write("Enter crystal type: ");
+        string crystal = Console.ReadLine();
+
+        if (!collection.ContainsKey(location))
+        {
+            collection[location] = new List<string>();
+        }
+
+        collection[location].Add(crystal);
+        Console.WriteLine($"Added {crystal} to {location}.");
+    }
 }
